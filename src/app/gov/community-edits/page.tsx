b7b2c07c027851e99016fc476289f8768d8ebd9e
Cole Bremer <alsetsolutionsinc@gmail.com>
2026-01-02 00:18:16 -0600

    Fix build error: remove invalid footerVariant prop from SimplePageLayout

--- src/app/gov/community-edits/page.tsx
+++ src/app/gov/community-edits/page.tsx
@@ -50,7 +50,7 @@ export default async function CommunityEditsPage() {
   }
 
   return (
-    <SimplePageLayout contentPadding="px-[10px] py-3" footerVariant="light">
+    <SimplePageLayout contentPadding="px-[10px] py-3">
       <div className="max-w-4xl mx-auto">
         {/* Breadcrumb Navigation */}
         <Breadcrumbs items={[
